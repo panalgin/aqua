20ed5ae38e2df0b55412f49d5165d499fa0a49a6
panalgin <mashadow@outlook.com>
2018-04-22 17:38:42 +0300

    controls are working

--- Aqua/MainController.cs
+++ Aqua/MainController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CefSharp;
 using CefSharp.WinForms;
+using System.Windows.Forms;
 
 namespace Aqua
 {
@@ -21,6 +22,33 @@ namespace Aqua
         {
             if (Parent.Browser != null)
                 Parent.Browser.ShowDevTools();
+        }
+
+        public void Close()
+        {
+            this.Parent.BeginInvoke(new Action(delegate ()
+            {
+                this.Parent.Close();
+            }));
+        }
+
+        public void ToggleWindow()
+        {
+            this.Parent.BeginInvoke(new Action(delegate ()
+            {
+                if (this.Parent.WindowState == FormWindowState.Normal)
+                    this.Parent.WindowState = FormWindowState.Maximized;
+                else
+                    this.Parent.WindowState = FormWindowState.Normal;
+            }));
+        }
+
+        public void Minimize()
+        {
+            this.Parent.BeginInvoke(new Action(delegate ()
+            {
+                this.Parent.WindowState = FormWindowState.Minimized;
+            }));
         }
     }
 }
---
+++ Aqua/View/js/control-box.js
@@ -0,0 +1,13 @@
+﻿$().ready(function () {
+    $("body").on("click", ".control-box #close", function (e) {
+        windowsApp.close();
+    });
+
+    $("body").on("click", ".control-box #max-normalize", function (e) {
+        windowsApp.toggleWindow();
+    });
+
+    $("body").on("click", ".control-box #minimize", function (e) {
+        windowsApp.minimize();
+    });
+});
